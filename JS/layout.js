8873682bee467ad622fe4d83ab3806b2a2b1c9dc
jorgegmch <226594601+jorgegmch@users.noreply.github.com>
2025-12-17 11:16:49 -0500

    feat: se modifican los nav de los demás HTML para compartir la propiedad responsive

--- JS/layout.js
+++ JS/layout.js
@@ -1,10 +1,10 @@
 // Nav responsive mobile
 
-const btnHamburguesa = document.querySelector(".nav-responsive");
+const btnResponsive = document.querySelector(".nav-responsive");
 const menu = document.querySelector(".menu-principal");
 const overlay = document.querySelector(".menu-overlay");
 
-btnHamburguesa.addEventListener("click", () => {
+btnResponsive.addEventListener("click", () => {
     menu.classList.toggle("activo");
     overlay.classList.toggle("activo");
 });
